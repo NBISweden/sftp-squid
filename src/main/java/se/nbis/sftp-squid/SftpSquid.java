47ef015dcc3b943def442fa1432141d34a6bc058
Johan Viklund <johan.viklund@bils.se>
2016-03-31 13:58:02 +0200

    [SftpSquid.java] Refactor

--- src/main/java/se/nbis/sftp-squid/SftpSquid.java
+++ src/main/java/se/nbis/sftp-squid/SftpSquid.java
@@ -21,109 +21,127 @@ import net.schmizz.sshj.common.StreamCopier;
 
 
 public class SftpSquid {
-    SSHClient ssh1;
-    SSHClient ssh2;
-    SFTPClient client1;
-    SFTPClient client2;
+    SSHClient[] ssh_clients;
+    SFTPClient[] sftp_clients;
+    HostFileInfo[] hfs;
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            System.out.println("Usage ....");
+        HostFileInfo[] hf = parseArgs(args);
+        SftpSquid ss = new SftpSquid(hf);
+        ss.run();
+    }
+
+    public static HostFileInfo[] parseArgs(String[] args) {
+        if (args.length < 2) {
+            System.err.println("You need to supply at least 2 servers");
             System.exit(1);
         }
-        HostFileInfo h1 = new HostFileInfo(args[0]);
-        HostFileInfo h2 = new HostFileInfo(args[1]);
-        SftpSquid ss = new SftpSquid();
+        HostFileInfo[] hf = new HostFileInfo[args.length];
+        for (int i=0; i < args.length ; i++) {
+            try {
+                hf[i] = new HostFileInfo(args[i]);
+            } catch (IOException e) {
+                System.err.printf("Malformed host argument \"%s\"\n", args[i]);
+                System.exit(1);
+            }
+        }
+        return hf;
+    }
+
+    SftpSquid(HostFileInfo[] hf) throws IOException {
+        if (hf.length < 2) {
+            throw new IOException("Need at least 2 hosts to transfer between");
+        }
+        this.hfs          = hf;
+        this.ssh_clients  = new SSHClient[hf.length];
+        this.sftp_clients = new SFTPClient[hf.length];
+    }
+
+    public void run() throws IOException {
         try {
-            try {
-                ss.connect1(h1.user, h1.host, h1.port);
-            } catch (UserAuthException e) {
-                System.out.println("Incorrect username and/or password for " + h1.userHostSpec());
-                throw e;
-            } catch (TransportException e) {
-                System.out.println("Something went wrong: " + e);
-                throw e;
-            } catch (ConnectException e) {
-                System.out.printf("Connection failure for : %s\n", h1.userHostSpec(), e.getMessage());
-                throw e;
-            }
-
-
-            try {
-                ss.connect2(h2.user, h2.host, h2.port);
-            } catch (UserAuthException e) {
-                System.out.println("Incorrect username and/or password for " + h2.userHostSpec());
-                throw e;
-            } catch (TransportException e) {
-                System.out.println("Something went wrong: " + e);
-                throw e;
-            } catch (ConnectException e) {
-                System.out.printf("Connection failure for : %s\n", h2.userHostSpec(), e.getMessage());
-                throw e;
-            }
-
-            ss.transferFile(h1.file, h2.file);
-        } finally {
-            ss.close();
+            connectAll();
+            transferFile();
+        }
+        catch (Exception e) {
+            usage();
+        }
+        finally {
+            closeAll();
         }
     }
 
-    public void close() throws IOException {
-        if ( client1 != null ) {
-            client1.close();
-        }
-        if ( client2 != null ) {
-            client2.close();
-        }
-        if ( ssh1 != null ) {
-            ssh1.close();
-        }
-        if ( ssh2 != null ) {
-            ssh2.close();
+    public void usage() {
+        System.out.println("Usage: sftp-squid <user1>@<server1>:<file_or_directory> <user2>@<server2>:<file_or_directory>");
+    }
+
+
+    public void connectAll() throws IOException {
+        for (int i=0; i<hfs.length; i++) {
+            try {
+                int tries = 3;
+                while (tries-- >= 0) {
+                    try {
+                        SSHClient ssh = connect(hfs[i]);
+                        SFTPClient sftp = ssh.newSFTPClient();
+
+                        ssh_clients[i] = ssh;
+                        sftp_clients[i] = sftp;
+                        tries = -1;
+                    } catch (UserAuthException e) {
+                        System.err.println("Incorrect username and/or password for " + hfs[i].userHostSpec() + " try again.");
+                    }
+                }
+            } catch (UserAuthException e) {
+                System.err.println("Incorrect username and/or password for " + hfs[i].userHostSpec());
+                throw e;
+            } catch (TransportException e) {
+                System.err.println("Something went wrong: " + e);
+                throw e;
+            } catch (ConnectException e) {
+                System.err.printf("Connection failure for %s: %s\n", hfs[i].userHostSpec(), e.getMessage());
+                throw e;
+            }
         }
     }
 
-    private SSHClient connect(String user, String host, int port)
-            throws UserAuthException, TransportException, IOException {
-
-        SSHClient ssh = new SSHClient();
-        ssh.addHostKeyVerifier(new PromiscuousVerifier());
-        ssh.connect(host, port);
-
-        ssh.auth(user, new AuthKeyboardInteractive(new UserKeyboardAuth()));
-        return ssh;
+    public void closeAll() throws IOException {
+        for (SFTPClient sftp_client : sftp_clients) {
+            if (sftp_client != null) {
+                sftp_client.close();
+            }
+        }
+        for (SSHClient ssh_client : ssh_clients) {
+            if (ssh_client != null) {
+                ssh_client.close();
+            }
+        }
     }
 
     private SSHClient connect(HostFileInfo hf)
             throws UserAuthException, TransportException, IOException {
-        return connect(hf.user, hf.host, hf.port);
+        SSHClient ssh = new SSHClient();
+        ssh.addHostKeyVerifier(new PromiscuousVerifier());
+        ssh.connect(hf.host, hf.port);
+
+        ssh.auth(hf.user, new AuthKeyboardInteractive(new UserKeyboardAuth(hf)));
+        return ssh;
     }
 
-    public void connect1(String user, String host, int port)
-            throws UserAuthException, TransportException, IOException {
+    public void transferFile() throws IOException {
+        RemoteFile fileFrom = sftp_clients[0].open(hfs[0].file);
+        // Check if destination is a directory
 
-        ssh1 = connect(user, host, port);
-        client1 = ssh1.newSFTPClient();
-    }
-
-    public void connect2(String user, String host, int port)
-            throws UserAuthException, TransportException, IOException {
-
-        ssh2 = connect(user, host, port);
-        client2 = ssh2.newSFTPClient();
-    }
-
-    public void transferFile(String from, String to) throws IOException {
-        RemoteFile fileFrom = client1.open(from);
-        RemoteFile fileTo   = client2.open(to, EnumSet.of(OpenMode.WRITE, OpenMode.CREAT, OpenMode.TRUNC));
+        RemoteFile fileTo   = sftp_clients[1].open(hfs[1].file, EnumSet.of(OpenMode.WRITE, OpenMode.CREAT, OpenMode.TRUNC));
 
         try {
             RemoteFile.ReadAheadRemoteFileInputStream streamFrom = fileFrom.new ReadAheadRemoteFileInputStream(16);
-            RemoteFile.RemoteFileOutputStream streamTo = fileTo.new RemoteFileOutputStream(0, 16);
+            RemoteFile.RemoteFileOutputStream streamTo           = fileTo.new RemoteFileOutputStream(0, 16);
+
             try {
                 StreamCopier sc = new StreamCopier(streamFrom, streamTo);
-                sc.bufSize(calculateBufSize(fileFrom, fileTo));
+                sc.bufSize(calculateMaxBufferSize(fileFrom, fileTo));
                 sc.keepFlushing(false);
+                sc.listener(new customListener(fileFrom.length()));
                 sc.copy();
             } finally {
                 streamFrom.close();
@@ -135,19 +153,49 @@ public class SftpSquid {
         }
     }
 
-    public int calculateBufSize(RemoteFile f1, RemoteFile f2) {
-        int remoteMaxPacketSize = client1.getSFTPEngine().getSubsystem().getRemoteMaxPacketSize();
-        if ( remoteMaxPacketSize > client2.getSFTPEngine().getSubsystem().getRemoteMaxPacketSize() ) {
-            remoteMaxPacketSize = client2.getSFTPEngine().getSubsystem().getRemoteMaxPacketSize();
+    /* This code is taken from the library docs and adjusted to match the
+     * situation we have with two remote servers */
+    private int calculateMaxBufferSize(RemoteFile f1, RemoteFile f2) {
+        int remoteMaxPacketSize = sftp_clients[0].getSFTPEngine().getSubsystem().getRemoteMaxPacketSize();
+        if ( remoteMaxPacketSize > sftp_clients[1].getSFTPEngine().getSubsystem().getRemoteMaxPacketSize() ) {
+            remoteMaxPacketSize = sftp_clients[1].getSFTPEngine().getSubsystem().getRemoteMaxPacketSize();
         }
         int packetOverhead = f2.getOutgoingPacketOverhead();
         return remoteMaxPacketSize - packetOverhead;
     }
 }
 
+class customListener implements StreamCopier.Listener {
+    long length = 0;
+    long nextPrint = 0;
+    long printDuration = 0;
+
+    customListener(long length) {
+        this.length = length;
+        printDuration = length/1000;
+    }
+
+    @Override
+    public void reportProgress(long transferred) throws IOException {
+        if (length != 0 && transferred == length) {
+            System.out.printf("\r100.0%% Done\n");
+        }
+        else if (nextPrint < transferred) {
+            nextPrint = transferred + printDuration;
+            System.out.printf("\r%5.1f%% ", 100 * (double) transferred/ (double) length);
+        }
+        //System.out.println("Transf: " + transferred);
+    }
+}
+
 class UserKeyboardAuth implements ChallengeResponseProvider {
     int n = 0;
     Resource r;
+    HostFileInfo hf;
+
+    UserKeyboardAuth(HostFileInfo hf) {
+        this.hf = hf;
+    }
 
     @Override
     public List<String> getSubmethods() {
@@ -163,7 +211,7 @@ class UserKeyboardAuth implements ChallengeResponseProvider {
 
     @Override
     public char[] getResponse(String prompt, boolean echo) {
-        System.out.printf("[%s] %s", r.getDetail(), prompt);
+        System.out.printf("[%s] %s", hf.userHostSpec(), prompt);
         Console cons = System.console();
         char[] resp = cons.readPassword();
         return resp;
